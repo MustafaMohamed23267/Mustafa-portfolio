008316a2db78a704a6d7fbfb9fc72f6be920bfba
Mustafa Mohamed <120753752+MustafaMohamed23267@users.noreply.github.com>
2025-12-19 18:00:11 +0200

    Update SkillsSection.jsx

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,21 +1,24 @@
 import { useState } from "react";
 
         const skills = [
-    {name:"html" , level:95 , category:"frontend"},
-    {name:"CSS" , level:90 , category:"frontend"},
-    {name:"React" , level:80 , category:"frontend"},
-    {name:"JavaScript" , level:85 , category:"frontend"},
-    {name:"tailwindcss" , level:92 , category:"frontend"},
-    {name:"Bootstrap" , level:98 , category:"frontend"},
+    {name:"html" ,  category:"frontend"},
+    {name:"CSS" ,  category:"frontend"},
+    {name:"React js" ,  category:"frontend"},
+    {name:"JavaScript" , category:"frontend"},
+    {name:"tailwindcss" , category:"frontend"},
+    {name:"Bootstrap" , category:"frontend"},
     
 
-    {name:"PHP" , level:90 , category:"Backend"},
-    {name:"Laravel" , level:88 , category:"Backend"},
-    {name:"MYSQL" , level:85 , category:"Backend"},
-    {name:"Rest Api" , level:80 , category:"Backend"},
+    {name:"PHP" ,  category:"Backend"},
+    {name:"Laravel" ,  category:"Backend"},
+    {name:"MYSQL" ,  category:"Backend"},
+    {name:"Restful Api" ,  category:"Backend"},
+    {name:"SQlite" ,  category:"Backend"},
 
-    {name:"GIT/GitHub" , level:70 , category:"tools"},
-    {name:"Vs Code" , level:100 , category:"tools"},
+    {name:"GIT/GitHub" ,  category:"tools"},
+    {name:"Vs Code" ,  category:"tools"},
+    {name:"Postman" ,  category:"tools"},
+    {name:"Herd" ,  category:"tools"},
     
 ];
 
@@ -49,12 +52,6 @@ export const SkillsSection = ()=>
                     {filterskills.map((skills,key)=> <div key={key} className="bg-card p-6 rounded-lg shadow-xs card-hover ">
                             <div className="flex flex-col">
                             <span className=" font-semibold texl-lg">{skills.name}</span>
-                                <div className="bg-red-500/50 w-full h-2 mt-1 rounded-full animate-skills overflow-hidden">
-                                    <div className=" bg-primary h-2 rounded-full" style={{width:skills.level +"%"}}/>
-                                </div>
-                                <div className="text-right mt-1">
-                                     <span className="text-md">{skills.level} %</span>
-                                </div>
 
                             </div>
 
