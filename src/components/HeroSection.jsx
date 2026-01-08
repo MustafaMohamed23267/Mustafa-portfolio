48b5abe3d1bf382c8a8fed318a5b9239005791ba
Mustafa Mohamed <120753752+MustafaMohamed23267@users.noreply.github.com>
2026-01-08 22:49:39 +0200

    update hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,11 +5,11 @@ export const HeroSection = ()=>{
     <section id="hero" className="relative min-h-screen flex flex-col justify-center items-center px-4 ">
         <div className=" max-w-4xl mx-auto z-10 text-center space-y-4 ">
             <h1 className="text-4xl md:text-6xl font-bold">
-                <span className=" fade-in">Hi, i am </span>
+                <span className=" fade-in">Hi, I am </span>
                 <span className="fill-gray-500 drop-shadow-xl/50 drop-shadow-gray-500/50 text-primary fade-in" >Mustafa</span>
                 <span className="text-linear-to-r/hsl from-red-500 to-blue-600"> Mohamed</span>
             </h1>
-            <p className="text-md md:text-xl text-gray-500 moving-animation">i am a fullstack web developer i creating website using html css and reactjs framework thats the front side the back side i use PHP Laravel RestApi and MYSQL</p>
+            <p className="text-md md:text-xl text-gray-500 moving-animation">I am a fullstack web developer I creating website using html css and reactjs framework thats the front side the back side I use PHP Laravel RestApi and MYSQL</p>
             <div className="moving-animation-delay-2 pt-4 ">
                 <a href="#projects" className="cosmic ">
                     View My Projects
