e5b9afae99eb8d953072aee005f0f8043a8611ca
Mustafa Mohamed <120753752+MustafaMohamed23267@users.noreply.github.com>
2026-08-12 15:43:38 +0300

    updatevite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/Mustafa-portfolio",
+  base: process.env.VITE_BASE_PATH || "/",
  
 })
